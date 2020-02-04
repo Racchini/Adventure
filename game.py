a9fa0f22f7a4217553a0147aa219b3a2c22a8912
Racchini <41713239+Racchini@users.noreply.github.com>
2020-02-04 12:52:22 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-#dank memes dank memes dank memes dank memes
 import random 
 
 class User:
@@ -23,108 +22,109 @@ def userFightStats(user, weapon, armor):
 	user.speed = 10 - (weapon.weight+armor.weight)
 	user.damage = (weapon.damage * weapon.skill)
 
-def smithy(verify):
-	if verify == "y":
-		upgradeWeapons(user1, sword)
-		
-	elif verify == "n":
-		print("Aww, ok.") 
-
-	else:
-		verify
 #visiting the blacksmith
 def upgradeWeapons(user, weapon):
-	print ("You have %d coins. One upgrade point costs one coin." %user.coins)
+	print ("You have %d coins. One upgrade point costs ten coins." %user.coins)
 	upgrade = str(input("Which would you like to upgrade: Damage or Weight? "))
 	if upgrade.lower() == "damage":
 		amnt = int(input("How many points would you like to upgrade " + upgrade + "? "))
-		if amnt <= user.coins:
+		if (amnt*10) <= user.coins:
 			for x in range(0, amnt): 			
 				weapon.damage += 1
-				user.coins -= 1
+				user.coins -= 10
 			print(weapon.name + "\'s damage is now %d" %weapon.damage)
 			print("You have %d coins remaining." %user1.coins)
-		elif amnt > user.coins:
+		elif (amnt*10) > user.coins:
 			print("Sorry, insufficient funds")
 			
-	if upgrade.lower() == "weight":
+	elif upgrade.lower() == "weight":
 		amnt = int(input("How many points would you like to upgrade " + upgrade + "? "))
-		if amnt <= user.coins:			
+		if (amnt*10) <= user.coins:			
 			for x in range(0, amnt):					
 				if weapon.weight == 1:
 					print("Weapon is now at minimum weight")
 					break
 				else:	
 					weapon.weight -= 1
-					user.coins -= 1
+					user.coins -= 10
 			print(weapon.name + "\'s weight is now %d." %weapon.weight)
 			print("You have %d coins remaining." %user1.coins)
-		elif amnt > user.coins:
+		elif (amnt*10) > user.coins:
 			print("Sorry, insufficient funds")
-	moreSmithing = str(input("anything else before you go? "))
+	elif choice.lower() == "stats":
+		print("Health: %d\nMoney: %d coins" %(user.health, user.coins))
+	
+	moreSmithing = str(input("Anything else before you go? "))
 	if moreSmithing == 'yes':
 		upgradeWeapons(user1,sword)
 	else:
+		print("Fare thee well, traveler.")
 		return
 		
 def inn(user):
-	innOptions = ["rent room", "buy meal", "chat with patrons"]			
-	innChoose = str(input("What would you like to do? \n>"))
-	if innChoose.lower() == "list":
+	innOptions = ["rent room (10 coins)", "buy meal (5 coins)", "chat with patrons"]			
+	innChoose = str(input("\nWhat would you like to do? \n>"))
+	if innChoose.lower() == "help":
 		for i in range(0,len(innOptions)):
 			print(innOptions[i])
 		inn(user)
 	elif innChoose.lower() == "rent room":
 		roomCost = 10
 		if user.coins < roomCost:
-			print("Insufficient coins")
+			print("Insufficient coin")
 			inn(user)
 		elif user.coins >= roomCost:
 			user.health += 15
-			user.coins -= 10
+			user.coins -= roomCost
 			print("You rent a room and sleep for the evening, waking up feeling refreshed.")	
 			inn(user)
 	elif innChoose.lower() == "buy meal":
 		mealCost = 5
 		if user.coins >= mealCost:
 			user.health += 5
-			user.coins -= 5
+			user.coins -= mealCost
 			print("Ah, delicious! You can feel the meal warm your belly.")
 			inn(user)
 		elif user.coins < mealCost:
 			print("A little light in the wallet, eh? Looks like you'll be skipping a meal today.")
 			inn(user)		
-	elif innChoose.lower() == "chat with patrons":
+	elif innChoose.lower() == "chat" or innChoose.lower() == "chat with patrons":
 		print("Politics, rumors, and gossip. Nothing useful right now.")
 		inn(user)
-	elif innChoose.lower() == "leave":
-		return	
-	elif innChoose.lower() == "wallet":
-		print(user.coins)
+	elif innChoose.lower() == "stats":
+		print("Health: %d \nMoney: %d coins" %(user.health,user.coins))
 		inn(user)
+	elif innChoose.lower() == "leave":
+		print("Come back again soon!")
+		return	
 	else:
 		inn(user)
 
 def fightSimulator(user, weapon):
 	enemyHealth = 25
+	totalDealt=0
+	totalReceived=0
 	while enemyHealth > 0:
 		ladyLuck = random.uniform(0.0, 1.0)
-		blowDealt = weapon.skill*weapon.damage*ladyLuck
-		blowReceived = 5*random.uniform(0.0,1.0)
+		blowDealt = (weapon.skill*weapon.damage-weapon.weight)*ladyLuck
+		blowReceived = 5*random.uniform(0.0,1.0)*weapon.skill
 		enemyHealth -= blowDealt
 		if enemyHealth > 0:
 			user.health -= blowReceived
+			totalReceived+=blowReceived
 		if user.health < 1:
 			print("You fainted!")
 			return
-	print("You are victorious!")	
-	user.coins += 5
-	weapon.skill += 0.1
+		totalDealt+=blowDealt
+	print("You are victorious! You clutch the purse with your winnings, exhausted.")
+	print("You dealt: %d damage \nYou received: %d damage" %(totalDealt,totalReceived))
+	weapon.skill += 0.2
+	user.coins += 25*int(weapon.skill)
 	return
 
 def training(user, weapon):
-	trainOptions = ["fight in the arena", "train", "leave"]
-	choice = str(input("What would you like to do? \n>"))
+	trainOptions = ["fight in the arena", "train (5 gold)", "leave"]
+	choice = str(input("\nWhat would you like to do? \n>"))
 	if choice.lower() == "fight":
 		fightSimulator(user, weapon)
 		if user.health < 1:
@@ -132,14 +132,20 @@ def training(user, weapon):
 		else:
 			training(user, weapon)
 	elif choice.lower() == "train":
-		weapon.skill += 0.1
-		user.coins -= 5
+		if user.coins>4:
+			weapon.skill += 0.1
+			user.coins -= 5
+			training(user,weapon)
+		else:
+			print("Trainers don't work for free, kid.")
+			training(user,weapon)
 	elif choice.lower()	== "leave":
+		print("As you exit, the roar of the statium fades.")
 		return
 	elif choice.lower() == "stats":
 		print("Health: %d\nMoney: %d coins" %(user.health, user.coins))
 		training(user,weapon)
-	elif choice.lower() == "list":
+	elif choice.lower() == "help":
 		for i in range(0,len(trainOptions)):
 			print(trainOptions[i])
 		training(user, weapon)
@@ -148,35 +154,43 @@ def training(user, weapon):
 	
 		
 sword = Weapon("sword", 5, 4)
-user1 = User(50, 100)				
+user1 = User(30, 100)				
 whatdo = " "
-print("Hello")
+print("Hello. 'Help' will show you your options. 'Stats' will show you your charater information. 'Leave' will leave your current location.")
 
 while whatdo != "quit":	
-	places = ["smithy", "town square", "inn", "training grounds", "leave"]				
+	print("\nWhat would you like to do?")
+	places = ["smithy", "inn", "training grounds", "leave"]				
 	whatdo = str(input(">"))
 	if whatdo.lower() == 'help':
 		print("Your options are:")
 		for i in range(0,len(places)):
 			print (places[i])
-	if whatdo.lower() == "smithy":
+	elif whatdo.lower() == "smithy":
 		upgradeWeapons(user1, sword)
-	if whatdo.lower() == "inn":
+	elif whatdo.lower() == "inn":
 		print("You walk into the inn. A fire crackles in the corner and you can smell the mutton they're serving today.")
 		inn(user1)
-	if whatdo.lower() == "training grounds":
-		print("Best of luck! \n \nYou walk into the training grounds. The smell of blood and beer hangs in the air")
+	elif whatdo.lower() == "training grounds":
+		print("Best of luck! \n \nYou walk into the training grounds. The smells of blood and beer hang in the air.")
 		training(user1, sword)
 		if user1.health <= 1:
-			print("You wake up in a bed at the in. You're bruised, but alive.")
-			user1.health = 15
-			user1.coins -= 20
-			inn(user1)
-	if whatdo.lower() == "money":
+			user1.coins -= 10
+			if user1.coins>0:
+				print("You wake up in a bed at the in. You're bruised, but alive. Perhaps some training would be in order.")
+				user1.health = 15
+				inn(user1)
+			else:
+				print("You wake up in a cell, your pockets empty. It's debtor's prison for you. Maybe in your next life you'll be more lucky...")
+				whatdo="quit"
+	elif whatdo.lower() == "money":
 		user1.coins += 10
 		print(user1.coins)
-	if whatdo.lower() == "stats":
+	elif whatdo.lower() == "stats":
 		print("Health: %d \nMoney: %d coins" %(user1.health,user1.coins))
-	if whatdo.lower() == "leave":
-		print("You venture forth from the small town.")
+	elif whatdo.lower() == "leave":
+		print("You venture forth from the small town...")
+		whatdo="quit"
+	else:
+		print("I'm sorry, I don't understand.")
 		
